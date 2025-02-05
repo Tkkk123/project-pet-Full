22bbb66a8afeffca7a3a61ce1206c4867bfc9ef4
Tk <1694334141@qq.com>
2025-02-05 20:12:26 +0800

    refactor: 清理项目，移除重复的 JavaScript 文件，保留 TypeScript 版本

--- project-pet/src/apis/PetList.js
+++
@@ -1,7 +0,0 @@
-import request from "@/utils/request";
-
-export const getPetListAPI = () => {
-  return request({
-    url: "/PetData",
-  });
-};
--- project-pet/src/apis/ScrollData.js
+++
@@ -1,6 +0,0 @@
-import request from "@/utils/request";
-export const ScrollApi = () => {
-  return request({
-    url: "/ScrollData",
-  });
-};
